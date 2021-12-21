a70bb363f3c3b4c945495b4dac9636fcb78881c9
Manos Fregkos <manos.fregkos@datawise.ai>
2021-12-21 12:19:59 +0200

    added hooks for login signup and logout

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGd0OA06IwHiexg9mGGj9txMTF1ZdhM7s",
+  authDomain: "projectmanagementtool-628da.firebaseapp.com",
+  projectId: "projectmanagementtool-628da",
+  storageBucket: "projectmanagementtool-628da.appspot.com",
+  messagingSenderId: "80620515495",
+  appId: "1:80620515495:web:943405d20456d3a6c92b82",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
