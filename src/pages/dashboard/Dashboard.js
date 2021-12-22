07c76747d68215a2d5b7bffbff7399d0002ecc5a
Manos Fregkos <manos.fregkos@datawise.ai>
2021-12-22 21:50:51 +0200

    content is finished

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,45 +1,47 @@
-import { useCollection } from '../../hooks/useCollection'
-import { useState } from 'react'
-import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from "../../hooks/useCollection";
+import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 // components
-import ProjectList from '../../components/ProjectList'
-import ProjectFilter from './ProjectFilter'
+import ProjectList from "../../components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
 
 // styles
-import './Dashboard.css'
+import "./Dashboard.css";
 
 export default function Dashboard() {
-  const { user } = useAuthContext()
-  const { documents, error } = useCollection('projects')
-  const [filter, setFilter] = useState('all')
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection("projects");
+  const [filter, setFilter] = useState("all");
 
   const changeFilter = (newFilter) => {
-    setFilter(newFilter)
-  }
-  
-  const projects = documents ? documents.filter(document => {
-    switch(filter) {
-      case 'all':
-        return true
-      case 'mine':
-        let assignedToMe = false
-        document.assignedUsersList.forEach(u => {
-          if(u.id === user.uid) {
-            assignedToMe = true
-          }
-        })
-        return assignedToMe
-      case 'development':
-      case 'design':
-      case 'sales':
-      case 'marketing':
-        console.log(document.category, filter)
-        return document.category === filter
-      default:
-        return true
-    }
-  }) : null
+    setFilter(newFilter);
+  };
+
+  const projects =
+    documents &&
+    documents.filter((document) => {
+      switch (filter) {
+        case "all":
+          return true;
+        case "mine":
+          let assignedToMe = false;
+          document.assignedUsersList.forEach((u) => {
+            if (u.id === user.uid) {
+              assignedToMe = true;
+            }
+          });
+          return assignedToMe;
+        case "development":
+        case "design":
+        case "sales":
+        case "marketing":
+          console.log(document.category, filter);
+          return document.category === filter;
+        default:
+          return true;
+      }
+    });
 
   return (
     <div>
@@ -48,5 +50,5 @@ export default function Dashboard() {
       {documents && <ProjectFilter changeFilter={changeFilter} />}
       {projects && <ProjectList projects={projects} />}
     </div>
-  )
+  );
 }
